3ae742ce6d308e58c5ae94b2a4931d1677132d6b
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-06-11 18:15:35 +0300

    add redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ContactForm } from './Form/Form';
@@ -6,25 +5,17 @@ import { ContactsList } from './Contacts/ContactList';
 import { nanoid } from 'nanoid';
 import { Filter } from './Filter/Filter';
 import { Container } from './Container/Container.styled';
+import { getContacts, getFilter } from 'redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact } from 'redux/contacts/contactsSlice';
+import { setFilter } from 'redux/filter/filterSlice';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const myContacts = JSON.parse(localStorage.getItem('contacts'));
-    setContacts(myContacts);
-  }, []);
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = ({ name, number }) => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+  
+  const handleSubmit = ({ name, number }) => {
     const newContact = { id: nanoid(), name, number };
     const checkUser = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
@@ -32,28 +23,28 @@ export const App = () => {
 
     checkUser
       ? toast.warn(`${name} is already in the contacts`)
-      : setContacts([newContact, ...contacts]);
+      : dispatch(addContact(newContact));
   };
 
   const handleChange = evt => {
-    setFilter(evt.currentTarget.value);
+    dispatch(setFilter(evt.currentTarget.value));
   };
 
   const getFilteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
-  const deleteContact = id => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
   };
   return (
     <Container>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
+      <ContactForm onSubmit={handleSubmit} />
       <Filter handleChange={handleChange} filter={filter} />
       <ContactsList
         contacts={getFilteredContacts}
-        onDeleteContact={deleteContact}
+        onDeleteContact={handleDeleteContact}
       />
       <ToastContainer />
     </Container>
